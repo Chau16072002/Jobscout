3aaac4deb6b3b1124198d88a808235812f61c138
NguyenTriBao <tribao0510@gmail.com>
2023-11-21 16:40:56 +0700

    carrer

--- wp-content/themes/jobscout/sections/cta.php
+++ wp-content/themes/jobscout/sections/cta.php
@@ -6,7 +6,22 @@
  */
 if( is_active_sidebar( 'cta' ) ){ ?>
 	<section id="cta-section" class="bg-cta-section">
-	    <?php dynamic_sidebar( 'cta' ); ?>
+	   <?php// dynamic_sidebar( 'cta' ); ?> 
+	   <section id="cta-section" class="bg-cta-section">
+	   <section id="raratheme_companion_cta_widget-1" class="widget widget_raratheme_companion_cta_widget">        
+        <div class="centered bttk-cta-bg" style="background:url(http://localhost:8080/Jobscout/wp-content/uploads/2019/01/StockSnap_E5GUBD8MWN-1.jpg) no-repeat; background-size: cover; background-position: center">
+            <div class="raratheme-cta-container">
+                <h2 class="widget-title" itemprop="name">CARRER WITH US</h2>                <div class="text-holder">
+                        <p>Plan Do see a global uis a hospitality. We strive to deliver unforgetable and bespoke exoeriences, to under stand local cultures like natives. to provide service that is warm but nit intrusive. and to foresee our guests' every need at all times. that is our sole mission and purpose.</p>
+<p>we are experts in all stages of project development: concept, design implementation and management.<br>
+we love to find unique way to create expriences that surpise and delight guests and customers.</p>
+                    <div class="button-wrap">
+                        <a style='background-color: transparent;border-color: #fff;' rel="noopener noexternal" target="_blank" href="https://rarathemes.com/wordpress-themes/jobscout-pro" class="btn-cta btn-1">MORE ABOUT US</a>                    </div>
+                </div>
+            </div> 
+        </div>        
+        </section> 
+	</section>
 	</section> <!-- .bg-cta-section -->
 <?php
 }
